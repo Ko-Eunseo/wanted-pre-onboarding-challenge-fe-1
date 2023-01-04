cd72481ffbb17120440a361f79a7acd4d7519b58
Ko-Eunseo <koyk0408@naver.com>
2023-01-04 16:26:49 +0900

    input 스타일 추가, value, name props 추가

--- src/components/common/Input.js
+++ src/components/common/Input.js
@@ -1,19 +1,38 @@
 import React from 'react';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const BORDER = {
+  border: css`
+    --input-border: 1px solid #eeeeee;
+  `,
+  transparent: css`
+    --input-border: none;
+  `,
+};
 
 const InputStyle = styled.input`
+  ${props => props.borderStyle};
   margin: 4px 0;
   padding: 8px;
-  border: 1px solid #eeeeee;
+  border: var(--input-border);
   width: 100%;
   border-radius: 4px;
 `;
 
-const Input = ({ id, type, onChange, placeholder }) => {
+const Input = ({ name, id, type, onChange, placeholder, border, value }) => {
+  const borderStyle = BORDER[border];
   return (
     <>
       <label htmlFor={id}></label>
-      <InputStyle id={id} type={type ? type : 'text'} onChange={onChange} placeholder={placeholder} />
+      <InputStyle 
+      id={id} 
+      name={name}
+      type={type ? type : 'text'} 
+      onChange={onChange} 
+      placeholder={placeholder} 
+      borderStyle={borderStyle}
+      value={value}
+      />
     </>
   )
 }
